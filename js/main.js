9248d354f96a860d721f662d6899adedd8e04ba5
ichi-e <chiemx8@gmail.com>
2024-11-08 21:08:38 +0900

    js更新

--- js/main.js
+++ js/main.js
@@ -103,13 +103,18 @@ function stopTimer() {
     timer2 = null;
 }
 
+
 // 記録
 function notes() {
+times.sort(function (first, second) {
+  return first - second;  
+});
+console.log(times);
     times.forEach(function (time, index) {
         let note = $("<div>")
             .data("index", index)
             .data("time", time)
-            .text((index+1) + "回目：" + time + "秒");
+            .text( "No" + (index+1) + " : " + time + "秒");
         $("#note").append(note);
     });
 }
